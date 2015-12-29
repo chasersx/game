d0503f02e779f0f2e774b93526f456b82aaef282
Dustin Lafley <chasersx@gmail.com>
2015-12-28 18:07:13 -0600

    Added WASD support, trying to add Life to screen

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var WATER_ROW = -12.50;
+var LIFE_NUMBER = 3;
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function(x, y) {//, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -9,7 +10,7 @@ var Enemy = function(x, y, speed) {
     // a helper we've provided to easily load images
 	this.x = x;
 	this.y = y;
-	this.speed = speed;
+	this.speed = Math.floor(Math.random() * (100 - 200) + 200);//speed;
     this.sprite = 'images/enemy-bug.png';
 	
 };
@@ -22,7 +23,7 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 	s = (Math.random() * 50 * dt);
     this.x += s;
-    if (this.x > 500) this.x = 0;
+    if (this.x > 480) this.x = 0;
     //this.call(this,x, y);
 };
 
@@ -33,9 +34,9 @@ Enemy.prototype.render = function() {
 
 var allEnemies = [];
 
-var enemy = new Enemy();
+//var enemy = new Enemy();
 for (i=0; i<=4; i++) {
-    enemy = new Enemy();
+  var enemy = new Enemy();
   allEnemies[i] = new Enemy();
   allEnemies.push(enemy);
 };
@@ -81,25 +82,25 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(direction){
-	if (this.x == 0 && direction === 'left'){
+	if (this.x == 0 && (direction === 'left' || direction === 'a')){
 		return;
 	}
-	if (this.x == 400 && direction === 'right'){
+	if (this.x == 400 && (direction === 'right' || direction === 'd')){
 		return;
 	}
-	if (this.y == 400 && direction === 'down'){
+	if (this.y == 400 && (direction === 'down' || direction === 's')){
 		return;
 	}
-	if(direction === 'left'){
+	if(direction === 'left' || direction === 'a'){
 		this.x -= 100;
 	}
-	if(direction === 'up'){
+	if(direction === 'up' || direction === 'w'){
 		this.y -= 82.5;
 	}
-	if(direction === 'right'){
+	if(direction === 'right' || direction === 'd'){
 		this.x += 100;
 	}
-	if(direction === 'down'){
+	if(direction === 'down' || direction === 's'){
 		this.y += 82.5;
 	}
  };
@@ -108,10 +109,26 @@ Player.prototype.handleInput = function(direction){
 	 this.x = 200;
 	 this.y = 400;
  }
+ 
+//Life class. Displays and tracks number of hearts/lives
+ var Life = function() {
 
- var player = new Player(200,-12.50);
+    this.number = LIFE_NUMBER;
+};
 
 
+//Draw the 3 hearts
+Life.prototype.render = function() {
+
+    for (var x = 0; x < this.number; x++) {
+        ctx.drawImage(Resources.get('images/Heart.png'), x * 45, 0);
+    }
+
+};
+ 
+ var player = new Player(200,400);
+ var life = new Life();
+ 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -119,7 +136,11 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+		87: 'w',
+		65: 'a',
+		83: 's',
+		68: 'd'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
