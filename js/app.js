c772fcf8457193b460b06233600e50a6ac680984
Dustin Lafley <chasersx@gmail.com>
2015-12-30 16:43:11 -0600

    Added collision, Hearts, dieing; enemies move more randomly
    Added two new Gems

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
+//Declare all variables that are going to be constant and hence, immutable.
 var WATER_ROW = -12.50;
 	LIFE_NUMBER = 3,
-	ENEMY_X_FORWARD = 0;
-
-//Declare all variables that are going to be constant and hence, immutable.
-var X_OFFSET = 15,
+	ENEMY_X_FORWARD = 0,
+	X_OFFSET = 15,
     Y_OFFSET = 83,
     HIDE = -101,
     Y_ARRAY = [63, 146, 229],
 	SPEED_ARRAY = [100,250,300,450],
-	RESET_ARRAY = [-100,-200,-50,-300],
-    PLAYER_X = 200,
-    PLAYER_Y = 400;
+	ENEMY_X_ARRAY = [-100,-200,-150,-300];
 
 /* Define Enemy class. All instances of the enemy class will acquire the 
    variables declared within the class.
 */
 var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
-    this.x = RESET_ARRAY[Math.floor(Math.random() * 4)];//ENEMY_X_FORWARD;
+    this.x = ENEMY_X_ARRAY[Math.floor(Math.random() * 4)];
     this.y = Y_ARRAY[Math.floor(Math.random() * 3)];
     this.speed =  SPEED_ARRAY[Math.floor(Math.random() * 4)];
+	this.width = 50;
+    this.height = 50;
 };
 
 Enemy.prototype.update = function(dt) {
@@ -27,7 +26,7 @@ Enemy.prototype.update = function(dt) {
 	this.x += this.speed * dt;
 	if (this.x < -500) {
         this.sprite = 'images/enemy-bug.png';
-        this.x = RESET_ARRAY[Math.floor(Math.random() * 4)];//ENEMY_X_FORWARD;
+        this.x = ENEMY_X_ARRAY[Math.floor(Math.random() * 4)];
         this.speed = SPEED_ARRAY[Math.floor(Math.random() * 4)];
         this.x += this.speed * dt;
         this.y = Y_ARRAY[Math.floor(Math.random() * 3)];
@@ -38,7 +37,8 @@ Enemy.prototype.update = function(dt) {
 };
 
 Enemy.prototype.reset = function() {
-	this.x = RESET_ARRAY[Math.floor(Math.random() * 4)];// ENEMY_X_FORWARD;
+	this.speed = SPEED_ARRAY[Math.floor(Math.random() * 4)];
+	this.x = ENEMY_X_ARRAY[Math.floor(Math.random() * 4)];
     this.y = Y_ARRAY[Math.floor(Math.random() * 3)];
  }
 
@@ -51,29 +51,21 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 //Player code------------------------------------------
-
 var Player = function(x,y) {
 	this.x = x;
 	this.y = y;
 	this.sprite = 'images/char-boy.png';
+	this.width = 50;
+    this.height = 50;
 };
 
 Player.prototype.update = function(dt) {
-	
-	
 	this.x * (dt);
 	this.y * (dt);
-	
 	if (this.y <= WATER_ROW){
 		player.reset();
-	}
-		
+	}	
 };
 
 // Draw the player on the screen, required method for game
@@ -109,22 +101,31 @@ Player.prototype.handleInput = function(direction){
 	 this.x = 200;
 	 this.y = 400;
  }
- 
-//Life class. Displays and tracks number of hearts/lives
- var Life = function() {
 
+//Life class. Displays and tracks number of hearts/lives
+var Life = function() {
+	//this.sprite = 'images/Heart.png';
     this.number = LIFE_NUMBER;
 };
-
 
 //Draw the 3 hearts
 Life.prototype.render = function() {
 
     for (var x = 0; x < this.number; x++) {
-        ctx.drawImage(Resources.get('images/Heart.png'), x * 45, 0);
+        ctx.drawImage(Resources.get('images/Heart.png'), x * 45, 525, 50, 70);
     }
 
 };
+
+Life.prototype.die = function() {
+	//if (this.number == 0)
+	//	gameOver();
+	this.number = this.number - 1;
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 
 var allEnemies = [];
 for(var i=0; i<3; i++){
@@ -134,6 +135,16 @@ allEnemies.push(new Enemy());
  var player = new Player(200,400);
  var life = new Life();
  
+ function checkCollisions(allEnemies, player) {
+    for(var i = 0; i < 3; i++) {
+		if (allEnemies[i].x < player.x + player.width && allEnemies[i].x + allEnemies[i].width > player.x &&
+            allEnemies[i].y < player.y +  player.height && allEnemies[i].height + allEnemies[i].y > player.y){
+				life.die()
+				player.reset();
+        }
+    }
+}
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
--- js/engine.js
+++ js/engine.js
@@ -81,6 +81,7 @@ var Engine = (function(global) {
     function update(dt) {
         updateEntities(dt);
         // checkCollisions();
+		checkCollisions(allEnemies,player);
     }
 
     /* This is called by the update function and loops through all of the
@@ -147,13 +148,12 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-		 
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 		
         player.render();
-		
+		life.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -173,7 +173,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+		'images/Heart.png'
     ]);
     Resources.onReady(init);
 
